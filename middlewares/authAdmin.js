a01e358b8c4ddac5a286e7f7e7a72490790849e6
devsalman247 <salman247.dev@gmail.com>
2022-09-13 14:29:15 +0500

    deleted unnecessary middleware

--- middlewares/authAdmin.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, res, next) {
-    const role = req.body.role;
-    if(role==="admin" || role==="staff") {
-        return next();
-    }else {
-        return res.send("Request is declined");
-    }
-}
