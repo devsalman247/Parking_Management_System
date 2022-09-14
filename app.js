3808cfa2aafcf2018eb72ca54488dd8acf31e528
devsalman247 <salman247.dev@gmail.com>
2022-09-14 18:36:53 +0500

    some fixes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express'),
     PORT = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://127.0.0.1:27017/parkingdb", () => {
+    app.listen(PORT, () => {
+        console.log(`Listening at port ${PORT}.`);
+    });
     console.log('Connected to database successfully');
 });
 
@@ -15,6 +18,3 @@ require('./models/Vehicle');
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => {
-    console.log(`Listening at port ${PORT}.`);
-});
